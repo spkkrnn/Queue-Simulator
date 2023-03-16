914a32bbb1c25558450ee725a37d80b0ef7a761d
spkrnn <sami.h.pekkarinen@gmail.com>
2023-03-16 03:24:25 +0200

    initial commit

---
+++ fifo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double rand_exp(double lambda) /* Returns a random number from the exponential distribution */
+{
+	double random = (double) rand() / RAND_MAX;
+	random = log(random);
+	double x = -random / lambda;
+	return x;
+}
+
+double avg_age(int n, double lambda,  double mu) /* Returns the average age of information */
+{
+	double time = 0;
+	double area = 0;
+	double last = 0;
+	double a, b, duration;
+	for (int i = 0; i < n; ++i)
+	{
+		a = time + rand_exp(lambda);
+		b = a + rand_exp(mu);
+		if (a < last) { b = last + rand_exp(mu); }
+		last = b;
+		duration = b - a;
+		area += (((a - time + duration) * (a - time + duration))/2) - ((duration * duration)/2);
+		time = a;
+	}
+	return (lambda*area/n);
+}
+
+int main(void)
+{
+	srand(time(NULL));
+	double arrivalRate = 0.5;
+	double serviceRate = 1;
+	double load = arrivalRate / serviceRate;
+	int arrivals = 1000000;
+
+	double AoI = avg_age(arrivals, arrivalRate, serviceRate);
+	double modelAoI = (1 + 1/load + (load*load)/(1 - load)) / serviceRate;
+	printf("Average age of information: %0.3f s\n", AoI);
+	printf("Average age according to formula: %0.3f s\n", modelAoI); 
+	return 0;
+}
